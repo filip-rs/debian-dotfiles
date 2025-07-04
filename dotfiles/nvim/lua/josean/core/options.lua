f3cd87959bb69fb48ed145de707b8359171b3e23
filip-rs <filip.spanne@gmail.com>
2025-07-04 09:35:12 +0200

    updated nvim to better syntax highlight go

--- dotfiles/nvim/lua/josean/core/options.lua
+++ dotfiles/nvim/lua/josean/core/options.lua
@@ -41,3 +41,12 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+vim.g.go_highlight_types = 1
+vim.g.go_highlight_fields = 1
+vim.g.go_highlight_functions = 1
+vim.g.go_highlight_function_calls = 1
+vim.g.go_highlight_operators = 1
+vim.g.go_highlight_extra_types = 1
+vim.g.go_highlight_build_constraints = 1
+vim.g.go_highlight_generate_tags = 1
